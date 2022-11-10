13fa05359564fd9aa570af111c41bfa2eeeb97d8
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-09 23:39:11 -0600

    got the app working

--- app.js
+++ app.js
@@ -2,32 +2,30 @@ const select = document.querySelector('select');
 const list = document.querySelector('ul');
 const h1 = document.querySelector('h1');
 
-let days = 31;
-const choice = select.value;
+select.addEventListener('change', () => {
+  const choice = select.value;
 
-select.addEventListener('change', findDays(days, choice));
-
-function findDays (days, choice) {
-    // add conditional here
-    
-    if (choice === 'febuary') {
-        days = 28;
-    } else if (choice === 'april' || choice ==='june' ||
-    choice === 'september' || choice === 'november') {
+  // ADD CONDITIONAL HERE
+  let days = 31;
+    if (choice === 'february') {
+        days = 28;  
+    } else if (choice === 'april' || choice === 'june' || choice === 'september' || choice === 'november') {
         days = 30;
     }
 
-    createCalendar(days, choice);
-};
+
+
+  createCalendar(days, choice);
+});
 
 function createCalendar(days, choice) {
-    list.innerHTML = '';
-    h1.textContent = choice;
-    for (let i = 1; i <= days; i++) {
-        const listItem = document.createElement('li');
-        listItem.textContent = i;
-        list.appendChild(listItem);
-    }
+  let choiceName = choice.charAt(0).toUpperCase() + choice.slice(1);  
+  list.innerHTML = '';
+  h1.textContent = choiceName;
+  for (let i = 1; i <= days; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = i;
+    list.appendChild(listItem);
+  }
 }
 
-createCalendar(days, choice);
